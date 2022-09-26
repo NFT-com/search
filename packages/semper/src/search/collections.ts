69235213548bcb6ce045abc35e33e8376d6158fb
t-morgan <tim@immutableholdings.com>
2022-09-26 13:51:06 -0400

    fix: include profile contract in collection scoring

--- packages/semper/src/search/collections.ts
+++ packages/semper/src/search/collections.ts
@@ -4,6 +4,12 @@ import { entity } from '@nftcom/shared'
 
 import { CollectionDao, NFTDao } from './model'
 
+const PROFILE_CONTRACT = process.env.TYPESENSE_HOST.startsWith('dev') ?
+  '0x9Ef7A34dcCc32065802B1358129a226B228daB4E' : '0x98ca78e89Dd1aBE48A53dEe5799F24cC1A462F2D'
+
+const GK_CONTRACT = process.env.TYPESENSE_HOST.startsWith('dev') ?
+  '0xe0060010c2c81A817f4c52A9263d4Ce5c5B66D55' : '0x8fB5a7894AB461a59ACdfab8918335768e411414'
+
 export const collectionNames = ['collections', 'nfts']
 
 const getRandomFloat = (min, max, decimals): number => {
@@ -12,6 +18,11 @@ const getRandomFloat = (min, max, decimals): number => {
   return parseFloat(str)
 }
 
+const calculateCollectionScore = (collection: CollectionDao): number => {
+  const officialVal = collection.isOfficial ? 1 : 0
+  const nftcomVal = [PROFILE_CONTRACT, GK_CONTRACT].includes(collection.contract) ? 1000000 : 0
+  return officialVal + nftcomVal
+}
 export const mapCollectionData = (
   collectionName: string,
   data: any[],
@@ -30,6 +41,7 @@ export const mapCollectionData = (
         description: collection.description || '',
         floor: process.env.TYPESENSE_HOST.startsWith('prod') ? 0.0 : getRandomFloat(0, 5, 2),
         nftType: collection.nft?.type || '',
+        score: calculateCollectionScore(collection),
       }
     })
     break
@@ -47,8 +59,6 @@ export const mapCollectionData = (
           }
         })
       }
-      const profileContract = process.env.TYPESENSE_HOST.startsWith('dev') ?
-        '0x9Ef7A34dcCc32065802B1358129a226B228daB4E' : '0x98ca78e89Dd1aBE48A53dEe5799F24cC1A462F2D'
       return {
         id: nft.id,
         nftName: nft.metadata.name || `${nft.collection?.name + ' '|| ''}#${tokenId}`,
@@ -65,7 +75,7 @@ export const mapCollectionData = (
         listedPx: process.env.TYPESENSE_HOST.startsWith('prod') ? 0.0 : getRandomFloat(0.3, 2, 2),
         currency: process.env.TYPESENSE_HOST.startsWith('prod') ? '' : 'ETH',
         status: '',
-        isProfile: nft.contract === profileContract,
+        isProfile: nft.contract === PROFILE_CONTRACT,
       }
     })
     break
