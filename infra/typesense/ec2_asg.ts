436f7df08e94b76413ab73079f07b43f37795c33
t-morgan <tim@immutableholdings.com>
2022-11-18 13:49:55 -0500

    fix: switch to arm64

--- infra/typesense/ec2_asg.ts
+++ infra/typesense/ec2_asg.ts
@@ -147,7 +147,7 @@ const getAmi = (): pulumi.Output<aws.ec2.GetAmiResult> => {
   return aws.ec2.getAmiOutput({
     filters: [{
       name: 'name',
-      values: ['ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*'], //hardcode ubuntu focal 20.04 as newer versions cause compatibility issues (tech debt)
+      values: ['ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*'],
     }],
     owners: ['099720109477'], // Canonical
     mostRecent: true,
