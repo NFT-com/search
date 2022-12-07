271f69c643cda0fe388a42e5a5d7de6735cd87f9
t-morgan <tim@immutableholdings.com>
2022-12-07 13:57:20 -0500

    chore: remove blank line

--- packages/semper/src/index.ts
+++ packages/semper/src/index.ts
@@ -43,7 +43,6 @@ const main = async (): Promise<void> => {
   // await commander.update('nfts', ['contractAddr'])
   // await commander.reindexNFTsByContract('0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB')
 
-
   await db.disconnect()
 }
 
